a828b9215de15e40921994da5c27ce4b12e817a1
Forhad Zinnah <forhadzinnah@gmail.com>
2023-08-18 08:43:04 -0700

    complete Chat Model

--- chat-backend/db/models/Chat.js
+++ chat-backend/db/models/Chat.js
@@ -1,2 +1,29 @@
 const {DataTypes} = require('sequelize')
 const db = require('../database')
+const User = require('./User')
+
+const Chat = db.define('Chat', {
+    user1_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id',
+        },
+    }, 
+    user2_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id',
+        },
+    },
+    last_message_timestamp: {
+        type: DataTypes.DATE
+    },   
+}, {
+    timestamps: true,
+})
+
+module.exports = Chat
