678b32ad70cdd02c8eb1a297ab2d206e5770fb95
Forhad Zinnah <forhadzinnah@gmail.com>
2023-10-27 17:45:12 -0700

    create thunk to getChats

--- chat-frontend/src/slices/chatSlice.js
+++ chat-frontend/src/slices/chatSlice.js
@@ -1,10 +1,42 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+// need to get a logged in users' active chats
+export const getChats = createAsyncThunk(
+  'getChats',
+  async (userId, { rejectWithValue }) => {
+    try {
+      let { data } = await axios.get(
+        `http://localhost:3001/api/chats/user/${userId}`
+      );
+      return data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
 const chatSlice = createSlice({
   name: 'chat',
-  initialState: {},
+  initialState: {
+    chats: [],
+    status: '',
+    error: '',
+  },
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getChats.fulfilled, (state, { payload }) => {
+      state.chats = payload;
+    });
+    builder.addCase(getChats.pending, (state) => {
+      state.status = 'loading';
+      state.error = '';
+    });
+    builder.addCase(getChats.rejected, (state, { payload }) => {
+      state.status = 'failed';
+      state.error = payload.message;
+    });
+  },
 });
 
 export const selectChat = (state) => state.chat;
