d81413fc05976d4a97c87079d51f667f837b1842
Forhad Zinnah <forhadzinnah@gmail.com>
2023-10-27 17:10:58 -0700

    initial setup

--- chat-frontend/src/slices/chatSlice.js
+++ chat-frontend/src/slices/chatSlice.js
@@ -1,2 +1,12 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+
+const chatSlice = createSlice({
+  name: 'chat',
+  initialState: {},
+  reducers: {},
+});
+
+export const selectChat = (state) => state.chat;
+
+export default chatSlice.reducer;
